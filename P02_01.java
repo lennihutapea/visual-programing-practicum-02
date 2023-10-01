2faf89438e2d54e5646fc9db47161c35277eec9c
lennihutapea <143679130+lennihutapea@users.noreply.github.com>
2023-10-01 12:11:04 +0700

    saya sudah selesai mengerjakan P02_01 dan P02_02

--- P02_01.java
+++ P02_01.java
@@ -1,1 +1,64 @@
-// NIM - Your Name
+// 12S23023 - Lenni febriyani
+
+import java.util.*;
+import java.lang.Math;
+
+class P02_01{
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String deskripsiTugas;
+
+        System.out.println("Deskripsi Tugas");
+        deskripsiTugas = input.nextLine();
+        String kodeMataKuliah;
+
+        System.out.println("Kode Mata Kuliah");
+        kodeMataKuliah = input.nextLine();
+        String namaMataKuliah;
+
+        System.out.println("Nama MatKul");
+        namaMataKuliah = input.nextLine();
+        String pengampu;
+
+        System.out.println("Nama Dosen Pengampu");
+        pengampu = input.nextLine();
+        String deadline;
+
+        System.out.println("Deadline");
+        deadline = input.nextLine();
+        String formatPenamaanTugas;
+
+        System.out.println("Format Penamaan Tugas");
+        formatPenamaanTugas = input.nextLine();
+        int tingkatkesulitan;
+
+        System.out.println("Tingkat kesulitan");
+        tingkatkesulitan = input.nextInt();
+        int hari;
+
+        System.out.println("Hari");
+        hari = input.nextInt();
+        String status;
+
+        System.out.println("Status");
+        status = input.nextLine();
+        double prioritas;
+        String hasil1, hasil2, hasil3;
+
+        prioritas = tingkatkesulitan * (1.0 / hari);
+        System.out.println("Prioritas : " + prioritas);
+        if (prioritas > 3) {
+            hasil1 = "Penting! Anda harus mengerjakan tugas ini segera.";
+        } else {
+            if (prioritas >= 1.5 && prioritas <= 3) {
+                hasil1 = "Tugas ini memiliki prioritas menengah.";
+            } else {
+                if (prioritas < 1.5) {
+                    hasil1 = "Tugas ini relatif ringan, namun jangan tunda terlalu lama.";
+                }
+            }
+        }
+        System.out.println(deskripsiTugas + "|" + kodeMataKuliah + "|" + namaMataKuliah + "|" + pengampu + "|" + deadline + "|" + formatPenamaanTugas + "|" + tingkatkesulitan + "|" + hari + "|" + status + "|" + hasil1);
+    }
+}
--- P02_02.java
+++ P02_02.java
@@ -1,1 +1,53 @@
-//NIM -Nama
+//12S23023 -Lenni febriyai
+
+import java.util.*;
+import java.lang.Math;
+
+class P02_02 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String namaGudang;
+
+        System.out.println("Masukkan Nama Gudang");
+        namaGudang = input.nextLine();
+        int kapasitasGudang;
+
+        System.out.println("Masukkan Kapasitas Gudang");
+        kapasitasGudang = input.nextInt();
+        int jumlahBukuSaatIni;
+
+        System.out.println("Masukkan Jumlah Buku Saat Ini");
+        jumlahBukuSaatIni = input.nextInt();
+        String aC;
+
+        System.out.println("AC?");
+        aC = input.nextLine();
+        String lantai;
+
+        System.out.println("Masukkan Jenis Lantai");
+        lantai = input.nextLine();
+        String ketersediaanTeknologi;
+
+        System.out.println("Bagaimana Ketersediaan Teknologi?");
+        ketersediaanTeknologi = input.nextLine();
+        int skor;
+
+        System.out.println("Masukkan skor");
+        skor = input.nextInt();
+        String hasil1;
+
+        if (skor >= 40) {
+            hasil1 = "Gudang Elite";
+        } else {
+            if (skor >= 25) {
+                hasil1 = "Gudang Standar";
+            } else {
+                if (skor < 25) {
+                    hasil1 = "Gudang perlu peningkatan";
+                }
+            }
+        }
+        System.out.println(namaGudang + "|" + kapasitasGudang + "|" + jumlahBukuSaatIni + "|" + aC + "|" + lantai + "|" + ketersediaanTeknologi + "|" + hasil1);
+    }
+}
